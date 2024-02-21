6f442ea18a7d8c27b1e25cca867d880f4170dcbd
cree-per-com <kcossld-naver.com>
2024-02-22 05:07:42 +0900

    로그인 기능 뼈대 구현

--- accountbook/src/main/java/com/example/accountbook/Controller/AccountController.java
+++ accountbook/src/main/java/com/example/accountbook/Controller/AccountController.java
@@ -1,2 +1,20 @@
+package com.example.accountbook.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class AccountController {
+
+    @GetMapping("/")
+    public String homePage() {
+        return "home";
+    }
+
+    @GetMapping( "/main")
+    public String mainPage() {
+        return "main";
+    }
 }

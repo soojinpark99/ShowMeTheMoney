185acaa45a087a02051aa67b4f941b915af700e0
cree-per-com <kcossld-naver.com>
2024-02-23 01:03:12 +0900

    [BE] ✨회원가입 DTO,컨트롤러,서비스 파일 생성

---
+++ accountbook/src/main/java/com/example/accountbook/Controller/JoinController.java
@@ -0,0 +1,8 @@
+package com.example.accountbook.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JoinController {
+
+}
---
+++ accountbook/src/main/java/com/example/accountbook/Service/JoinService.java
@@ -0,0 +1,8 @@
+package com.example.accountbook.Service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JoinService {
+}

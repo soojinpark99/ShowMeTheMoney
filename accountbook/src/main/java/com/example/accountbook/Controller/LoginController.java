0482e1cd88a47a3f2773a972bb31afef4caf53d8
cree-per-com <kcossld-naver.com>
2024-02-24 13:01:59 +0900

    [BE]✏️ url을 수정하여 다른 사용자의 main페이지로 접근하는 것 차단

--- accountbook/src/main/java/com/example/accountbook/Controller/LoginController.java
+++ accountbook/src/main/java/com/example/accountbook/Controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.example.accountbook.Controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -10,16 +14,12 @@ import java.security.Principal;
 
 @Controller
 public class LoginController {
-    @GetMapping("/")
-    public String homePage() {
-        return "home";
-    }
 
     @GetMapping("/login")
     public String loginPage() {return "login";}
 
     @GetMapping("/main/username")
-    public ResponseEntity<Void> currentUserName(Principal principal) {
+    public ResponseEntity<Void> UserName(Principal principal) {
         String username = principal.getName();
         String redirectUrl = "/main/username/"+username;
         return ResponseEntity.status(HttpStatus.FOUND)
@@ -28,9 +28,19 @@ public class LoginController {
                 .build();
     }
 
+    @Autowired
+    private HttpServletRequest request;
 
     @GetMapping( "/main/username/{username}")
     public String mainPage(@PathVariable String username) {
+      //url 보안검증방식과 세션 관리 방식 중 url 보안검증 채택
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        if(!username.equals(currentUsername)) {
+            return "redirect:/error";
+        }
+
+
         return "main";
     }
 

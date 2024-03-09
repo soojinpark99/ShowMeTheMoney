6d53d99d6d29efbbe1ccf2d385ae73df3fe1c0bf
ichaehwa <coghk777@gmail.com>
2024-03-09 11:16:10 +0900

    머지용 커밋

--- accountbook/src/main/java/com/example/accountbook/DAO/CalendarDTO.java
+++ accountbook/src/main/java/com/example/accountbook/DAO/CalendarDTO.java
@@ -3,18 +3,21 @@ package com.example.accountbook.DAO;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class CalendarDTO {
     private int id;
-    private String date; //yyyy-mm-dd
+    private LocalDate date; //yyyy-mm-dd
     private String division; //income or expense
     private int money;
     private String category;
     private String memo;
 
-    //추가
+    /*
     private int month;
     private int day;
     private int calid;
+    */
 }
